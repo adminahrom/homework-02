a150656bc1e6ead48831efa419923c416586a33f
Vladimir Sharikov <adminahrom@mail.ru>
2021-08-17 00:47:58 +0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import ReactDom from "react-dom";
+
+const element = <h1>Hello World меня зовут: Владимир Шариков</h1>
+
+ReactDom.render(element, document.getElementById('root'))
